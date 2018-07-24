32436c238a03231fcd41f3c209531ae97971c601
Johnathan Kastner <johnathan.kastner@gmail.com>
2018-07-24 14:11:06 -0400

    #Xamarin.Forms corner radius bug. First checkin.

---
+++ XFRoundButtonBug/XFRoundButtonBug/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFRoundButtonBug
+{
+
+    using Xamarin.Forms;
+    class MainViewModel : BindableObject
+    {
+        private int theButtonCornerRadius = 25;
+        public int TheButtonCornerRadius
+        {
+            get => this.theButtonCornerRadius;
+            set
+            {
+                if (this.theButtonCornerRadius != value)
+                {
+                    this.theButtonCornerRadius = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+    }
+}
